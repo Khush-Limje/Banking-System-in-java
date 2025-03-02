fcbfa177e34282b27a1ba663c4328694650bab3b
Khush-Limje <khushallimje247@gmail.com>
2025-03-02 17:47:49 +0530

    Changes in files.

--- BankingSystem.java
+++ BankingSystem.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class BankingSystem {
 
-    public static void main(String[] lund){
+    public static void main(String[] args){
         String name ;
         Long phone;
             
